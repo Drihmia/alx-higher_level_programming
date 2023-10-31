f8022a7e8319df2f887f2a75bd2f2086123aa9af
Drihmia <drihmia.redouane@gmail.com>
2023-10-31 18:44:10 +0100

    removed the sys module

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-import sys
-
 
 def islower(c):
     if not c:
         print("Traceback (most recent call last): you have type anything")
-        sys.exit()
+        return False
     elif (c >= 'a' and c <= 'z'):
         return True
     return False
