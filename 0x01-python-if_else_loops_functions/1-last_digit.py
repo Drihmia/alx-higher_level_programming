be40a3048f938b8738c5d90a6fa60dfeb7faf307
Drihmia <drihmia.redouane@gmail.com>
2023-10-31 22:13:26 +0100

    used short format version instead of format m ethode

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of {:=-d} is ".format(number), end="")
+print(f"Last digit of {number} is ", end="")
 
 if number < 0:
     number = number % 10 - 10
@@ -11,5 +11,5 @@ if number == 0:
     print(str(number) + " and is 0")
 elif number > 5:
     print(str(number) + " and is greater than 5")
-elif number < 6:
+else:
     print(str(number) + " and is less than 6 and not 0")
