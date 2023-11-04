a349413f0b39a9cacef6cc55b5720e4e49d1a67d
Drihmia <drihmia.redouane@gmail.com>
2023-11-04 23:48:59 +0100

    fixed some edge cases

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for (i, ch) in enumerate(my_string):
-        if ch in "cC":
-            my_string = my_string[:i] + my_string[i + 1:]
-    return my_string
+    str1 = ""
+    for ch in my_string:
+        if ch not in "cC":
+            str1 = str1 + ch
+    return str1
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_a = len(tuple_a)
-    len_b = len(tuple_b)
-    if len_b < 2:
-        if len_b == 1:
-            tuple_b = tuple_b + (0, )
-        tuple_b = tuple_b + (0, 0)
-    elif len_a < 2:
-        if len_a == 1:
-            tuple_a = tuple_a + (0, )
-        tuple_a = tuple_a + (0, 0)
-    t1 = tuple_a[0] + tuple_b[0]
-    t2 = tuple_a[1] + tuple_b[1]
-    return t1, t2
+    if tuple_a and tuple_b:
+        len_a = len(tuple_a)
+        len_b = len(tuple_b)
+        if len_b < 2:
+            if len_b == 1:
+                tuple_b = tuple_b + (0, )
+            tuple_b = tuple_b + (0, 0)
+        if len_a < 2:
+            if len_a == 1:
+                tuple_a = tuple_a + (0, )
+            tuple_a = tuple_a + (0, 0)
+        t1 = tuple_a[0] + tuple_b[0]
+        t2 = tuple_a[1] + tuple_b[1]
+        return t1, t2
