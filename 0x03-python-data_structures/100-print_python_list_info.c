6d02465f2088bb9cd5931542c4ebd537722ff1ed
Drihmia <drihmia.redouane@gmail.com>
2023-11-06 01:13:52 +0100

    added a check of the pyobject given to the funtion is a list object, but not an instance of a subtype of the list type.

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,12 +10,13 @@ void print_python_list_info(PyObject *p)
 	Py_ssize_t size = 0, i;
 	PyObject *item = NULL;
 
-	size = PyList_Size(p);
-	if (!size)
+	if (!PyList_CheckExact(p))
 		return;
 
-		printf("[*] Size of the Python List = %zu\n", size);
-		printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+	size = PyList_Size(p);
+	printf("[*] Size of the Python List = %zu\n", size);
+
+	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
 	for (i = 0; i < size; i++)
 	{
 		item = PyList_GetItem(p, i);
