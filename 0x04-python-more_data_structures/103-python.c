f1fede81c7d950520c3909a912215457c26cebcc
Drihmia <drihmia.redouane@gmail.com>
2023-11-09 02:00:54 +0100

    added the second funtion

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,4 +1,5 @@
 #include <Python.h>
+void print_python_bytes(PyObject *p);
 
 /**
  * print_python_list - prints some basic info about Python lists.
@@ -10,18 +11,53 @@ void print_python_list(PyObject *p)
 	PyListObject *cast = (PyListObject *)p;
 	Py_ssize_t listSize = ((PyVarObject *)p)->ob_size;
 
-	printf("[*] Allocated = %zd\n", cast->allocated);
+	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %zd\n", cast->ob_base.ob_size);
+	printf("[*] Allocated = %zd\n", cast->allocated);
 
 	for (Py_ssize_t i = 0; i < listSize; i++)
 	{
 		PyObject *item = ((PyListObject *)p)->ob_item[i];
 
-		if (item == NULL)
-			printf("Element %zd is NULL\n", i);
-		else
+		if (item)
+		{
 			printf("Element %zd: %s\n", i, ((PyTypeObject *)(item->ob_type))->tp_name);
+			if (PyBytes_Check(item))
+				print_python_bytes(item);
+		}
 	}
 }
 
 
+/**
+ * print_python_bytes - prints some basic info about Python bytes objects.
+ * @p: python object
+ * Return: NONE.
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size;
+	const char *data;
+	Py_ssize_t i;
+
+	printf("[.] bytes object info\n");
+	if (PyBytes_Check(p))
+	{
+		size = ((PyVarObject *)p)->ob_size;
+		data = ((PyBytesObject *)p)->ob_sval;
+
+		printf("  size: %zd\n", size);
+		printf("  trying string: %s\n", data);
+		size++;
+		printf("  first %zd bytes:", (size < 10) ? size : 10);
+
+
+		for (i = 0; i < ((size < 10) ? size : 10); i++)
+		{
+			printf(" %02x", (unsigned char)data[i]);
+		}
+		printf("\n");
+	}
+	else
+		printf("  [ERROR] Invalid Bytes Object\n");
+}
