55671f713205feb9d4233526b246f7394dde14ac
Drihmia <drihmia.redouane@gmail.com>
2023-11-22 06:07:59 +0100

    used self.__radius and make radius optionnal

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -11,7 +11,7 @@ class MagicClass:
         radius: integer or float : or radias of a circle.
     """
 
-    def __init__(self):
+    def __init__(self, radius=0):
         """Initialize the MagicClass instance.
 
         Raises:
@@ -23,7 +23,7 @@ class MagicClass:
 
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self._MagicClass__radius = radius
+        self.__radius = radius
 
     def area(self):
         """Compute the area of a given circl.
@@ -32,7 +32,7 @@ class MagicClass:
             float : the current circl area.
         """
 
-        return self._MagicClass__radius ** 2 * math.pi
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """Compute the circumference of a given circl.
@@ -41,4 +41,4 @@ class MagicClass:
             float : the current circl circumference.
         """
 
-        return 2 * math.pi * self._MagicClass__radius
+        return 2 * math.pi * self.__radius
