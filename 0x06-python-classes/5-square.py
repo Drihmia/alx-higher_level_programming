41461b48ecb4ea304e469691c76257a6e6c7a88a
Drihmia <drihmia.redouane@gmail.com>
2023-11-22 03:01:54 +0100

    updated the doc strings

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,25 +1,49 @@
 #!/usr/bin/python3
 
+
+"""Documentation for Square class """
+
+
 class Square:
-    """
-    A class Square that defines a square.
+    """Empty class Square that defines a square."""
 
-
-    the size nust be an integer and greater or equal to 0.
-    """
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        """Initialize the Square instance.
+
+        Args:
+            size (int): The size of the square. Must be a non-negative integer.
+
+        Raises:
+            ValueError: If the given size is negative.
+            TypeError: If the given size is not an integer.
+        """
+
+        self.size = size
 
     @property
     def size(self):
+          """Get or set the size of the square.
+
+        Returns:
+            int: The current size of the square.
+        """
+
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size for the square.
+                Must be a non-negative integer.
+
+        Raises:
+            ValueError: If the given size is negative.
+            TypeError: If the given size is not an integer.
+
+        """
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
