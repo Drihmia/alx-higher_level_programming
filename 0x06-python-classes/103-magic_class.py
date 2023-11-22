c409988f3141f91c79c84f49513a8d79f55b0704
Drihmia <drihmia.redouane@gmail.com>
2023-11-22 05:12:46 +0100

    magic class task, we have been given the byte code and we had to findwrite the source code for it

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""class magic for byte code"""
+
+
+class MagicClass:
+
+    """Class magic class for byte code.
+
+    Attributes:
+        radius: integer or float : or radias of a circle.
+    """
+    radius = 0
+
+    def __init__(self):
+        """Initialize the MagicClass instance.
+
+        Raises:
+            TypeError: if the given radius is not a float or and int.
+
+        """
+
+        self._MagicClass__radius = 0
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError('radius must be a number')
+        else:
+            self._MagicClass__radius = radius
+
+    def area(self):
+        """Compute the area of a given circl.
+
+        Returns:
+            float : the current circl area.
+        """
+        return self._MagicClass__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Compute the circumference of a given circl.
+
+        Returns:
+            float : the current circl circumference.
+        """
+        return 2 * math.pi * self._MagicClass__radius
