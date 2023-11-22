0cf7b364ea39cda86620f210a48d4ebf24f912ec
Drihmia <drihmia.redouane@gmail.com>
2023-11-22 02:40:21 +0100

    fixed the documentations

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 
+
 class Square:
-    """
-    A class Square that defines a square.
+    """Empty class Square that defines a square."""
 
-    Args:
-        size (int) : The first field.
-
-    the size nust be an integer and greater or equal to 0.
-    """
     def __init__(self, size=0):
+        """Initialize the Square instance.
+
+        Args:
+            size (int): The size of the square. Must be a non-negative integer.
+
+        Raises:
+            ValueError: If the given size is negative.
+            TypeError: If the given size is not an integer.
+        """
+
         if isinstance(size, int):
             if size >= 0:
                 self.__size = size
