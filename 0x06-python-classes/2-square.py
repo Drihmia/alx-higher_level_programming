40ed3ebb407dba4616d2aeee80a70e7234ec8330
Drihmia <drihmia.redouane@gmail.com>
2023-11-22 02:26:57 +0100

    add raises to init

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 
+"""Documentation for Square class """
+
+
 class Square:
-    """
-    A class Square that defines a square.
+    """Empty class Square that defines a square."""
 
-    Args:
-        size (int) : The first field.
+    def __init__(self, size):
+        """Initialize the Square instance.
 
-    the size nust be an integer and greater or equal to 0.
-    """
-    def __init__(self, size=0):
+         Args:
+            size (int): The size of the square. Must be a non-negative integer.
+
+        Raises:
+            ValueError: If the given size is negative.
+            TypeError: If the given size is not an integer.
+
+        """
         if isinstance(size, int):
             if size >= 0:
                 self.__size = size
