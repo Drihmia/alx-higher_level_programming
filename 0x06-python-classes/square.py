f920ce79c74de1e4171d92259d3dcd930b91d72e
Drihmia <drihmia.redouane@gmail.com>
2023-11-22 02:07:15 +0100

    made all files executables

--- 0x06-python-classes/square.py
+++ 0x06-python-classes/square.py
@@ -4,22 +4,13 @@ class Square:
     """
     A class Square that defines a square.
 
+    Args:
+        size (int) : The first field.
 
     the size nust be an integer and greater or equal to 0.
     """
     def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
-
-        if not isinstance(position, tuple) or len(position) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(position[0], int) or not isinstance(position[1], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
 
     # setting and getting the size field.
@@ -44,13 +35,10 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
+        if isinstance(value, tuple) and value[0] >= 0 and value[1] >= 0:
+            self.__position = value
+        else:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int) or not isinstance(value[1], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
     def area(self):
         """
@@ -67,10 +55,7 @@ class Square:
 
         If size is equal to 0, print an empty line.
         """
-        if self.size == 0:
-            print()
-            return
-        for i in range(self.__size + self.position[1]):
+        for i in range(self.size + self.position[1]):
             if i < self.position[1]:
                 print()
             else:
@@ -79,4 +64,3 @@ class Square:
                 for j in range(self.size + self.position[0]):
                     print(" " if j < self.position[0] else "#", end="")
         print()
-
