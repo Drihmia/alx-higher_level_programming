953c72aa2f09d04acb03e065249a9c1908f0ad29
Drihmia <drihmia.redouane@gmail.com>
2023-11-27 23:28:40 +0100

    added costring for the classe and separated shebang line from module's docstring

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 this module contains one funtion that handle different aspect of a rectangle
     such as the area and the perimeter...
@@ -10,4 +11,6 @@ this module contains one funtion that handle different aspect of a rectangle
 
 
 class Rectangle:
+    """Class Rectangle that define a rectangle"""
+
     pass
