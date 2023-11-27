95e494ae8563b0d389c159a6042432cce4e0aa75
Drihmia <drihmia.redouane@gmail.com>
2023-11-27 23:26:51 +0100

    1st commit and push to check docstring are being accepted before reusing same file to the next task

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this module contains one funtion that handle different aspect of a rectangle
+    such as the area and the perimeter...
+
+>>> rec = Rectangle()
+>>> type(rec)
+<class '...Rectangle'>
+"""
+
+
+class Rectangle:
+    pass
