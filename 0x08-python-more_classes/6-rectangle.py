90f64c08bcf2b51f36b0d3945ccff7580bae3d11
Drihmia <drihmia.redouane@gmail.com>
2023-11-28 02:29:47 +0100

    added a Public class attribute number_of_instances: Initialized to 0 Incremented during each new instance instantiation Decremented during each instance deletion

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,238 @@
+#!/usr/bin/python3
+
+"""
+this module contains one funtion that handle different aspect of a rectangle
+    such as the area and the perimeter...
+
+>>> rec = Rectangle()
+>>> type(rec)
+... # doctest +ELLIPSIS
+<class '...Rectangle'>
+"""
+
+
+class Rectangle:
+    """Class Rectangle that define a rectangle"""
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """Initialize the Rectangle instance.
+
+        >>> my_rectangle = Rectangle(2, 4)
+        >>> "{:d} instances of Rectangle".format(Rectangle.number_of_instances)
+        '1 instances of Rectangle'
+        >>> dictio = my_rectangle.__dict__
+        >>> dictio["_Rectangle__height"] == 4
+        True
+        >>> dictio["_Rectangle__width"] == 2
+        True
+        >>> my_rectangle = Rectangle(5)
+        Bye rectangle...
+        >>> "{:d} instances of Rectangle".format(Rectangle.number_of_instances)
+        '1 instances of Rectangle'
+        >>> dictio = my_rectangle.__dict__
+        >>> dictio["_Rectangle__height"] == 0
+        True
+        >>> dictio["_Rectangle__width"] == 5
+        True
+        """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Get the value of width
+
+        Returns:
+            int : the width's value
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the value of width
+        Raises:
+            TypeError: width must be an integer.
+            ValueError: width must be >= 0.
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the value of height
+
+        Returns:
+            int : the height's value
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the value of height
+        Raises:
+            TypeError: height must be an integer.
+            ValueError: height must be >= 0.
+        """
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Compute the area of the rectangle
+
+        >>> my_rectangle = Rectangle(2, 4)
+        >>> print("Area: {}".format(my_rectangle.area()))
+        Area: 8
+        >>> my_rectangle = Rectangle(0, 4)
+        Bye rectangle...
+        >>> print("Area: {}".format(my_rectangle.area()))
+        Area: 0
+        >>> my_rectangle = Rectangle(2, 0)
+        Bye rectangle...
+        >>> print("Area: {}".format(my_rectangle.area()))
+        Area: 0
+        >>> my_rectangle = Rectangle(2)
+        Bye rectangle...
+        >>> print("Area: {}".format(my_rectangle.area()))
+        Area: 0
+        >>> my_rectangle = Rectangle()
+        Bye rectangle...
+        >>> print("Area: {}".format(my_rectangle.area()))
+        Area: 0
+        """
+
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Compute the perimeter of the rectangle
+
+        Returns:
+            int : positive value
+        >>> my_rectangle = Rectangle(2, 4)
+        >>> print("perimeter: {}".format(my_rectangle.perimeter()))
+        perimeter: 12
+        >>> my_rectangle = Rectangle(0, 4)
+        Bye rectangle...
+        >>> print("perimeter: {}".format(my_rectangle.perimeter()))
+        perimeter: 0
+        >>> my_rectangle = Rectangle(4, 0)
+        Bye rectangle...
+        >>> print("perimeter: {}".format(my_rectangle.perimeter()))
+        perimeter: 0
+        >>> my_rectangle = Rectangle(4)
+        Bye rectangle...
+        >>> print("perimeter: {}".format(my_rectangle.perimeter()))
+        perimeter: 0
+        >>> my_rectangle = Rectangle()
+        Bye rectangle...
+        >>> print("perimeter: {}".format(my_rectangle.perimeter()))
+        perimeter: 0
+        """
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        """Implementing the __str__ magic method so that the object
+            return a rectangle of squares.
+
+        >>> my_rectangle = Rectangle(2, 4)
+        >>> print(str(my_rectangle))
+        ##
+        ##
+        ##
+        ##
+        >>> print(repr(my_rectangle))
+        Rectangle(2, 4)
+        >>> my_rectangle = Rectangle(0, 4)
+        Bye rectangle...
+        >>> print(str(my_rectangle))
+        <BLANKLINE>
+        >>> print(repr(my_rectangle))
+        Rectangle(0, 4)
+        """
+        symbol = "#"
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        square = (symbol * self.__width + "\n") * self.__height
+        return square[:-1]
+
+    def __repr__(self):
+        """Implementing the __repr__ magic method so that the object
+            return a string representation of the rectangle to be able
+            to recreate a new instance by using eval() (see example below).
+
+        >>> my_rectangle = Rectangle(2, 4)
+        >>> print(str(my_rectangle))
+        ##
+        ##
+        ##
+        ##
+        >>> print(my_rectangle)
+        ##
+        ##
+        ##
+        ##
+        >>> print(repr(my_rectangle))
+        Rectangle(2, 4)
+        >>> print(hex(id(my_rectangle)))
+        ... # doctest +ELLIPSIS
+        0x...
+        >>> new_rectangle = eval(repr(my_rectangle))
+        >>> print(str(new_rectangle))
+        ##
+        ##
+        ##
+        ##
+        >>> print(str(new_rectangle))
+        ##
+        ##
+        ##
+        ##
+        >>> print(repr(new_rectangle))
+        Rectangle(2, 4)
+        >>> print(hex(id(new_rectangle)))
+        ... # doctest +ELLIPSIS
+        0x...
+        >>> print(new_rectangle is my_rectangle)
+        False
+        >>> print(type(new_rectangle) is type(my_rectangle))
+        True
+        """
+        h = self.__height
+        w = self.__width
+        return "Rectangle(" + str(w) + ", " + str(h) + ")"
+
+    def __del__(self):
+        """Impliment the __del__ magic methode to print
+            Bye rectangle... to stdout while deleting
+            is captured
+        >>> my_rectangle = Rectangle(2, 4)
+        >>> l = "Area: {} - Perimeter: {}"
+        >>> print(l.format(my_rectangle.area(), my_rectangle.perimeter()))
+        Area: 8 - Perimeter: 12
+        >>> del my_rectangle
+        Bye rectangle...
+        >>> print(my_rectangle)
+        Traceback (most recent call last):
+        NameError: name 'my_rectangle' is not defined
+        """
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(optionflags=doctest.ELLIPSIS | doctest.FAIL_FAST)
