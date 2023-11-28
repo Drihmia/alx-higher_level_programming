997bba80dbf41f8c0f2e878e231bbcb24370aca0
Drihmia <drihmia.redouane@gmail.com>
2023-11-28 14:51:19 +0100

    removed a forgotten test print in the code

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -259,7 +259,6 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        print(rect_1.area(), rect_2.area())
         if rect_1.area() > rect_2.area():
             return rect_1
         elif rect_1.area() < rect_2.area():
