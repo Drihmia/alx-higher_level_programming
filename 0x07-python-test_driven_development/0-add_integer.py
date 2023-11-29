7dd1ae52f2c0317075532f2865bb718aba9d6d12
Drihmia <drihmia.redouane@gmail.com>
2023-11-29 18:02:07 +0100

    undoing last one was good move, return int a if b is overflowing

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -44,7 +44,7 @@ def add_integer(a, b=98):
     if a == float('inf') or a == -float('inf'):
         return 98
     if b == float('inf') or b == -float('inf'):
-        return 0
+        return int(a)
 
     result = int(a) + int(b)
     if result == float('inf') or result == -float('inf'):
