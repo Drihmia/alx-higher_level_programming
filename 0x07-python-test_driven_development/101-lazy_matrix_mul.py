fa2d7c55bb0789b7ed4e8bfaf282e2b33acb5ab8
Drihmia <drihmia.redouane@gmail.com>
2023-12-01 23:23:06 +0100

    made text as optional parameter

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-from numpy import matmul
-
 """
 this module contains one funtion that multiplies 2 matrices by
 using the module NumPy
 
+>>> lazy_matrix_mul([[1, 2, 3], [4, 5, 6]], [[7, 8], [9, 10], [11, 12]])
+array([[ 58,  64],
+       [139, 154]])
 """
+from numpy import matmul
 
 
 def lazy_matrix_mul(m_a, m_b):
