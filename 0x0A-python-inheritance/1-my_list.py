129211b1436616fa8a6c2564e187c7ada67e6d78
Drihmia <drihmia.redouane@gmail.com>
2023-12-04 17:39:47 +0100

    docsrings

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """
 This module contains one funtion that a class that inherits from list
-
 """
 
 
 class MyList(list):
-
     """MyList (child class) inherits from list class (parent class)"""
 
     def print_sorted(self):
