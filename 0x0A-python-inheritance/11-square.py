e47068c6f5857161cb6e353d5951c3cb7dcc055b
Drihmia <drihmia.redouane@gmail.com>
2023-12-04 23:53:08 +0100

    Write a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py).

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+this module contain a classe called Square that inherits
+f**m Base Rectangle class
+"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """A classe that inherits f**m BaseGeometry (7-base_geometry.py)
+
+    """
+
+    def __init__(self, size):
+        """
+        Instantiation with size
+
+        Args:
+            size : integer
+        Raises:
+            ValueError:  integer_validator;
+                size must be positive integers
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculating the area for the rectangle
+            method that has been BaseGeometry
+        Calculating the area for the rectangle
+            method that has been BaseGeometry
+
+        Return : the area as integer.
+
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        A string methode that return a frinedla msg
+        A string methode that return a frinedla msg
+        A string methode that return a frinedla msg
+
+        """
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
