fa56255892660afe1aa721f4c11baad3eba1496f
Drihmia <drihmia.redouane@gmail.com>
2023-12-04 22:09:09 +0100

    removing word from

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
 this module contain a classe called Rectangle that inherits
-from Base Geometry class
+f**m Base Geometry class
+
 """
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
-    """  a classe that inherits from BaseGeometry (7-base_geometry.py)"""
+    """A classe that inherits f**m BaseGeometry (7-base_geometry.py)
+
+    
+    """
 
     def __init__(self, width, height):
         """
         Instantiation with width and height
 
         Raises:
-            ValueError: from integer_validator;
+            ValueError: f**m integer_validator;
                 width and height must be positive integers
         """
 
