4cb3dde9e4eb7bf36c8dcda65c39bb91b3be3011
Drihmia <drihmia.redouane@gmail.com>
2023-12-04 22:17:11 +0100

    Write a class Rectangle that inherits from BaseGeometry (7-base_geometry.py). (task based on 8-rectangle.py), the area() method must be implemented , print() should print, and str() should return, the following rectangle description: [Rectangle] <width>/<height>

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""
+this module contain a classe called Rectangle that inherits
+f**m Base Geometry class
+"""
+
+
+class Rectangle(BaseGeometry):
+    """A classe that inherits f**m BaseGeometry (7-base_geometry.py)
+
+    """
+
+    def __init__(self, width, height):
+        """
+        Instantiation with width and height
+
+        Raises:
+            ValueError: f**m integer_validator;
+                width and height must be positive integers
+        """
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        ha = self.__class__.__name__
+        return "[{}] {}/{}".format(ha, self.__width, self.__height)
