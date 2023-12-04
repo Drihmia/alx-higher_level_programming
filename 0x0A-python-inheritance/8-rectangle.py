d5e8a8d8a9fa28d4b40b93056db8aafea1337ed6
Drihmia <drihmia.redouane@gmail.com>
2023-12-04 22:03:59 +0100

    add docstring for init methtod

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,6 +8,13 @@ class Rectangle(__import__("7-base_geometry").BaseGeometry):
     """  a classe that inherits from BaseGeometry (7-base_geometry.py)"""
 
     def __init__(self, width, height):
+        """Instantiation with width and height
+
+        Raises:
+            ValueError: from integer_validator;
+                width and height must be positive integers
+        """
+
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
