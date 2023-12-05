ba76df5cf3b43664fe11b33f48793e705b551ad5
Drihmia <drihmia.redouane@gmail.com>
2023-12-05 01:45:04 +0100

    fixed some docstrings

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+this module contain funtion that add new attribute to a object if its possible
+"""
+
 
 def add_attribute(obj, name, value):
+    """Return  the name of attribute that has been added by this function
+    Raise:
+        TypeError : can't add new attribute is it is not possible
+    """
+
     if '__slots__' in dir(obj) or "__dict__" not in dir(obj):
         raise TypeError("can't add new attribute")
     obj.name = value
