1675353a82a341a3b5e634adc658ab8e20b25256
Drihmia <drihmia.redouane@gmail.com>
2023-12-05 20:21:06 +0100

    added extra parameter to to_json method

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This module contain a class called Student
+"""
+
+
+class Student:
+    """ A class Student that defines a student by:
+    Args:
+        first_name (str) : name of student.
+        last_name (str) : last name of student.
+        age (int) : age of student.
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, lst=[]):
+        """
+        A method that retrieves a dictionary representation of a
+        Student instance (same as 8-class_to_json.py).
+
+        Args:
+            lst (list) : a list if attributes
+
+        - If attrs is a list of strings, only attribute names contained in
+        this list must be retrieved.
+        - Otherwise, all attributes must be retrieved
+
+        """
+        if type(lst) is list:
+            if not len(lst):
+                return self.__dict__
+            dic = {}
+            for ele in lst:
+                try:
+                    getattr(self, ele)
+                except Exception as e:
+                    continue
+                atr = getattr(self, ele)
+                if atr:
+                    dic[ele] = atr
+            return dic
