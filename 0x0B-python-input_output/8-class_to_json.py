5f9e92ab0c9d0fa0c2b6c2a694d5ac279658cec2
Drihmia <drihmia.redouane@gmail.com>
2023-12-05 18:38:20 +0100

    a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object:

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Modulle that contain a function called class_to_json that takes
+a Python object
+"""
+
+
+def class_to_json(obj):
+    """
+    A a function that returns the dictionary description with simple data
+    structure (list, dictionary, string, integer and boolean) for JSON
+    serialization of an object:
+    """
+    par = dir(obj)
+    dic = {}
+    for ele in par:
+        attr = getattr(obj, ele)
+        if isinstance(attr, (int, str, float))\
+                and "doc" not in ele and "module" not in ele:
+            dic[ele] = attr
+    return dic
