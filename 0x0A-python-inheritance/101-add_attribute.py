d9a5bbd7583068d10137e7c478ffe31eb04a95f9
Drihmia <drihmia.redouane@gmail.com>
2023-12-05 01:49:24 +0100

    iused setattr function istead of normal assignement-

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -12,5 +12,4 @@ def add_attribute(obj, name, value):
 
     if '__slots__' in dir(obj) or "__dict__" not in dir(obj):
         raise TypeError("can't add new attribute")
-    obj.name = value
-    return obj.name
+    setattr(obj, name, value)
