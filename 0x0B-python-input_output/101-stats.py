412e6561e6bc305613ce8796ec87c727ffe8843e
Drihmia <drihmia.redouane@gmail.com>
2023-12-06 02:18:33 +0100

    a script that reads stdin line by line and computes metrics:

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this a script that reads fron stdin line by line and computes metrics.
+"""
+import sys
+
+
+dic = {}
+total_size = 0
+while True:
+    N_lines = 0
+    for lin in sys.stdin:
+        N_lines += 1
+        line = lin.split()
+        total_size += int(line[-1])
+        tmp = line[-2]
+        if tmp in dic:
+            dic[tmp] += 1
+        else:
+            dic[tmp] = 1
+        if N_lines >= 10:
+            break
+    for key, value in dic.items():
+        print(str(key) + ":", value)
+    print("File size:", total_size)
