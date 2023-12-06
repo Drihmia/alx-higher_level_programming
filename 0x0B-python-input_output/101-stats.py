e5199f95500bfa6a7dfb1327170c16a765a4e4f0
Drihmia <drihmia.redouane@gmail.com>
2023-12-06 03:11:49 +0100

    put the script inside a funtion and used gard is name = main

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -6,22 +6,31 @@ this a script that reads fron stdin line by line and computes metrics.
 import sys
 
 
-dic = {}
-total_size = 0
-while True:
-    N_lines = 0
-    for lin in sys.stdin:
-        N_lines += 1
-        line = lin.split()
-        total_size += int(line[-1])
-        tmp = line[-2]
-        if tmp in dic:
-            dic[tmp] += 1
-        else:
-            dic[tmp] = 1
-        if N_lines >= 10:
-            break
-    dic = dict(sorted(dic.items()))
-    for key, value in dic.items():
-        print(str(key) + ":", value)
-    print("File size:", total_size)
+def main():
+    """
+    main funtion : entry point
+    """
+
+    dic = {}
+    total_size = 0
+    while True:
+        N_lines = 0
+        for lin in sys.stdin:
+            N_lines += 1
+            line = lin.split()
+            total_size += int(line[-1])
+            tmp = line[-2]
+            if tmp in dic:
+                dic[tmp] += 1
+            else:
+                dic[tmp] = 1
+            if N_lines >= 10:
+                break
+        dic = dict(sorted(dic.items()))
+        for key, value in dic.items():
+            print(str(key) + ":", value)
+        print("File size:", total_size)
+
+
+if __name__ == "__main__":
+    main()
