a58e708aac1e58bd4695bfe24287c5cdfbaacfe3
Drihmia <drihmia.redouane@gmail.com>
2023-12-06 03:59:29 +0100

    e

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -13,30 +13,33 @@ def main():
 
     dic = {}
     total_size = 0
-    while True:
-        N_lines = 0
-        for lin in sys.stdin:
-            N_lines += 1
+    try:
+        while True:
+            N_lines = 0
+            for lin in sys.stdin:
+                N_lines += 1
+                try:
+                    line = lin.split()
+                    total_size += int(line[-1])
+                    tmp = line[-2]
+                    if tmp in dic:
+                        dic[tmp] += 1
+                    else:
+                        dic[tmp] = 1
+                except Exception:
+                    pass
+                if N_lines >= 10:
+                    break
             try:
-                line = lin.split()
-                total_size += int(line[-1])
-                tmp = line[-2]
-                if tmp in dic:
-                    dic[tmp] += 1
-                else:
-                    dic[tmp] = 1
+                dic = dict(sorted(dic.items()))
             except Exception:
-                continue
-            if N_lines >= 10:
-                break
-        try:
-            dic = dict(sorted(dic.items()))
-        except Exception:
-            continue
-        finally:
-            print("File size:", total_size)
-            for key, value in dic.items():
-                print(str(key) + ":", value)
+                pass
+            finally:
+                print("File size:", total_size)
+                for key, value in dic.items():
+                    print(str(key) + ":", value)
+    except KeyboardInterrupt:
+        print("File size:", total_size)
 
 
 if __name__ == "__main__":
