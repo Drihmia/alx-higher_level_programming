30c34697973cdd84aa7b5a4243e65fb1736cc2d3
Drihmia <drihmia.redouane@gmail.com>
2024-01-18 19:22:58 +0100

    removed line 5 et 7

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,6 +2,4 @@
 -- You need to convert all of the following to UTF8:
 -- Database hbtn_0c_0, Table first_table, Field name in first_table
 USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
