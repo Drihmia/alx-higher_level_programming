2a652e26fc8a6232c1d086a67ba944ee3a7d700a
Drihmia <drihmia.redouane@gmail.com>
2024-02-18 21:51:12 +0100

    add importing lines inside the condition if the file is called as the main file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,11 +5,12 @@ no argument validation needed.
 
 Usage: script <mysql UserName> <mysql PassWord> <mysql DatabaseName>
 """
-import MySQLdb
-from sys import argv
 
 
 if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
 
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,11 +7,12 @@ Usage: script\
         <mysql UserName> <mysql PassWord> <mysql DatabaseName>\
         <mysql StateName> (searched)
 """
-import MySQLdb
-from sys import argv
 
 
 if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
 
