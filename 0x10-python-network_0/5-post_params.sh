3b9a4d926beccc89d1611bc0be6fbd4f50bb6f2b
Drihmia <drihmia.redouane@gmail.com>
2024-03-01 02:05:38 +0100

    updates

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL as an argument, sends a POST request to
-curl -s -X "POST" -d "email: test@gmail.com" -d "subject: I will always be here for PLD" "$1"
+curl -s -X "POST" -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -16,7 +16,15 @@ def find_peak(list_of_integers):
     """a function that finds a peak in a list of unsorted integers."""
 
     if list_of_integers:
-        for i in range(1, len(list_of_integers)):
-            if (list_of_integers[i] >= list_of_integers[i - 1] and
-                list_of_integers[i] >= list_of_integers[i + 1]):
-                return  list_of_integers[i]
+        low = 0
+        high = len(list_of_integers) - 1
+        
+        while low < high:
+            mid = (low + high) // 2
+            
+            if list_of_integers[mid] > list_of_integers[mid + 1]:
+                high = mid
+            else:
+                low = mid + 1
+                
+        return list_of_integers[low]
