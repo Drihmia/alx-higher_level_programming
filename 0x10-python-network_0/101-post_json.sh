0a1fa4531aed3d933f6b37cc6048b1a502dd5d45
Drihmia <drihmia.redouane@gmail.com>
2024-03-01 01:21:46 +0100

    g

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a JSON POST request to a URL passed as the first argument,
-# and displays the size of the body of the response
-
-curl -H "Content-Type: application/json" -d @"$2" "$1"
+curl -s -H "Content-Type: application/json" -d @"$2" "$1"
