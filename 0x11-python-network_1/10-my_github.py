d1bfb5526fef8692f6a73f0b46a5de37688c05f1
Drihmia <drihmia.redouane@gmail.com>
2024-03-02 01:36:55 +0100

    3rd commit task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+a Python script that takes your GitHub credentials (username and
+password) and uses the GitHub API to display your id
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data['id']
+        print(user_id)
+    else:
+        print("None")
+
+    # 2nd method using the json method
+    # text = response.json()
+    # print(text.get('id', None))
