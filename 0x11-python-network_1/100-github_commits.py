479b2b76e48cb91a2cbf6e1752cf346af0060354
Drihmia <drihmia.redouane@gmail.com>
2024-03-04 20:40:30 +0100

    updates my code and well exploited the api page in github and start finally have a better understanding how api works

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,26 +4,28 @@ a Python script that takes your GitHub credentials (username and
 password) and uses the GitHub API to display your id
 """
 import sys
-import requests
+from requests import get
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-
-    url = 'https://api.github.com/user'
-    response = requests.get(url, auth=(username, password))
-
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    headers = {
+            "Accept": "application/vnd.github+json"
+            }
+    params = {
+            "page": 1,
+            "per_page": 10
+            }
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner_name,
+                                                              repo_name)
+    response = get(url, params=params, headers=headers)
     try:
-        if response.status_code == 200:
-            user_data = response.json()
-            user_id = user_data['id']
-            print(user_id)
-        else:
-            print("None")
-    except Exception as f:
+        user_datas = response.json()
+        for user_data in user_datas:
+            sha = user_data.get("sha")
+            author_name = user_data.get("commit").get("author").get("name")
+            print(sha + ":", author_name)
+    except KeyError as f:
         print("None")
-
-    # 2nd method using the json method
-    # text = response.json()
-    # print(text.get('id', None))
